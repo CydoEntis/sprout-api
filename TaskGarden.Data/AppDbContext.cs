0a8be559af4372ff6dcb2853c4bfe18ebbb072e1
Cydo <cydoentis@gmail.com>
2025-01-14 16:06:45 -0500

    Added data project, and setup user class and application db context.

---
+++ TaskGarden.Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskGarden.Data.Models;
+using TaskGarden.Data.Models;
+
+namespace TaskGarden.Data;
+
+public class AppDbContext : IdentityDbContext<User> 
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
