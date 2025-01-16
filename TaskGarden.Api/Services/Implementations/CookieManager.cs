9fffb1f13a0e5ef44fe22fd9f664473542527711
Cydo <cydoentis@gmail.com>
2025-01-16 17:42:34 -0500

    Feature: Added CookieManager.cs to help simplify cookie management.

---
+++ TaskGarden.Api/Services/Implementations/CookieManager.cs
@@ -0,0 +1,33 @@
+﻿namespace TaskGarden.Api.Services.Implementations;
+
+public class CookieManager
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CookieManager(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public void Append(string name, string value, bool httpOnly, DateTime expiry)
+    {
+        var options = new CookieOptions
+        {
+            HttpOnly = httpOnly,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = expiry
+        };
+        _httpContextAccessor.HttpContext.Response.Cookies.Append(name, value, options);
+    }
+
+    public string Get(string name)
+    {
+        return _httpContextAccessor.HttpContext.Request.Cookies[name];
+    }
+
+    public void Delete(string name)
+    {
+        _httpContextAccessor.HttpContext.Response.Cookies.Delete(name);
+    }
+}
