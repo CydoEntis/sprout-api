2a012909e34cd69dad9fd9db25ea0e8a84904d6b
Cydo <cydoentis@gmail.com>
2025-01-17 18:06:09 -0500

    Feat: Added Project related constants to ProjectConsts.cs

---
+++ TaskGarden.Api/Constants/ProjectConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskGarden.Api.Constants;
+
+public class ProjectConsts
+{
+    public const string ProjectApi = "TaskGarden.Api";
+    public const string ProjectData = "TaskGarden.Data";
+    public const string EmailDir = "Email";
+    public const string EmailTemplatesDir = "Templates";
+}
---
+++ TaskGarden.Api/Services/Implementations/EmailManager.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskGarden.Api.Services.Implementations;
+
+public class EmailManager
+{
+    private readonly string _templateDirectory;
+
+    public EmailManager()
+    {
+        var solutionDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\"));
+        _templateDirectory = Path.Combine(solutionDirectory, , "Emails", "Templates");
+    }
+}
