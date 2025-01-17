e7729e8f0aa12baf43fc8a185303401e854f1ff4
Cydo <cydoentis@gmail.com>
2025-01-17 18:00:51 -0500

    Feat: Added ChangePasswordRequestDto.cs, ChangePasswordResponseDto.cs, ForgotPasswordRequestDto.cs, ResetPasswordRequestDto.cs

---
+++ TaskGarden.Api/Dtos/Auth/ChangePasswordRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskGarden.Api.Dtos.Auth;
+
+public class ChangePasswordRequestDto
+{
+    public string CurrentPassword { get; set; }
+    public string NewPassword { get; set; }
+}
---
+++ TaskGarden.Api/Dtos/Auth/ForgotPasswordRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskGarden.Api.Dtos.Auth;
+
+public class ForgotPasswordRequestDto : ResponseDto
+{
+    public string Email { get; set; }
+}
