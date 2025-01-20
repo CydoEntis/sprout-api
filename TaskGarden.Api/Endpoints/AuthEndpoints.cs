1e04bc89ba419fd316487e5e055fc553c3d7dd5a
Cydo <cydoentis@gmail.com>
2025-01-19 23:17:40 -0500

    Refactor: Updated Login and Register endpoints to use new ApiResponse model.

--- TaskGarden.Api/Endpoints/AuthEndpoints.cs
+++ TaskGarden.Api/Endpoints/AuthEndpoints.cs
@@ -11,71 +11,72 @@ public static class AuthEndpoints
         var group = routes.MapGroup("/api/auth").WithTags("Auth");
 
         group.MapPost("/register", async (RegisterRequestDto registerRequestDto, IAuthManager authManager) =>
-        {
-            var response = await authManager.Register(registerRequestDto);
-            return Results.Ok(ApiResponse<>);
-        })
-        .WithName("Register")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status200OK);
+            {
+                var response = await authManager.Register(registerRequestDto);
+                return Results.Ok(ApiResponse<AuthenticatedResponseDto>.SuccessResponse(response));
+            })
+            .WithName("Register")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK);
 
         group.MapPost("/login", async (LoginRequestDto loginRequestDto, IAuthManager authManager) =>
-        {
-            var response = await authManager.Login(loginRequestDto);
-            return Results.Ok(response);
-        })
-        .WithName("Login")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status200OK);
+            {
+                var response = await authManager.Login(loginRequestDto);
+                return Results.Ok(ApiResponse<AuthenticatedResponseDto>.SuccessResponse(response));
+            })
+            .WithName("Login")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK);
 
         group.MapPost("/refresh-tokens", async (ICookieManager cookieManager, IAuthManager authManager) =>
-        {
-            var response = await authManager.RefreshTokens();
-            return Results.Ok(response);
-        })
-        .WithName("RefreshTokens")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status200OK);
+            {
+                var response = await authManager.RefreshTokens();
+                return Results.Ok(response);
+            })
+            .WithName("RefreshTokens")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status200OK);
 
         group.MapPost("/logout", async (ICookieManager cookieManager, IAuthManager authManager) =>
-        {
-            var response = await authManager.Logout();
-            return Results.Ok(response);
-        })
-        .WithName("Logout")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status200OK);
+            {
+                var response = await authManager.Logout();
+                return Results.Ok(response);
+            })
+            .WithName("Logout")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status200OK);
 
         group.MapPost("/forgot-password", async (ForgotPasswordRequestDto requestDto, IAuthManager authManager) =>
-        {
-            var response = await authManager.ForgotPasswordAsync(requestDto);
-            return Results.Ok(response);
-        })
-        .WithName("ForgotPassword")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status200OK);
+            {
+                var response = await authManager.ForgotPasswordAsync(requestDto);
+                return Results.Ok(response);
+            })
+            .WithName("ForgotPassword")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status200OK);
 
         group.MapPost("/reset-password", async (ResetPasswordRequestDto requestDto, IAuthManager authManager) =>
-        {
-            var response = await authManager.ResetPasswordAsync(requestDto);
-            return Results.Ok(response);
-        })
-        .WithName("ResetPassword")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status200OK);
+            {
+                var response = await authManager.ResetPasswordAsync(requestDto);
+                return Results.Ok(response);
+            })
+            .WithName("ResetPassword")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status200OK);
 
-        group.MapPost("/change-password", async (string userId, ChangePasswordRequestDto requestDto, IAuthManager authManager) =>
-        {
-            var response = await authManager.ChangePasswordAsync(userId, requestDto);
-            return Results.Ok(response);
-        })
-        .WithName("ChangePassword")
-        .Produces(StatusCodes.Status400BadRequest)
-        .Produces(StatusCodes.Status404NotFound)
-        .Produces(StatusCodes.Status200OK);
+        group.MapPost("/change-password",
+                async (string userId, ChangePasswordRequestDto requestDto, IAuthManager authManager) =>
+                {
+                    var response = await authManager.ChangePasswordAsync(userId, requestDto);
+                    return Results.Ok(response);
+                })
+            .WithName("ChangePassword")
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status200OK);
     }
 }
