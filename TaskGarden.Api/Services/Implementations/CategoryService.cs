74fb87555dfbdc00cc26f71b68723b34737a127f
Cydo <cydoentis@gmail.com>
2025-01-21 16:00:43 -0500

    Feat: Added CategoryService.cs and ICategoryService.cs

---
+++ TaskGarden.Api/Services/Implementations/CategoryService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TaskGarden.Api.Services.Contracts;
+using TaskGarden.Data.Repositories.Contracts;
+
+namespace TaskGarden.Api.Services.Implementations;
+
+public class CategoryService : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IMapper _mapper;
+
+    public CategoryService(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+
+}
