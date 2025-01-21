f39170889f24822db45d9daf5d943ab5189dd8be
Cydo <cydoentis@gmail.com>
2025-01-21 17:09:05 -0500

    Refactor: Added missing relationship between Categories and AppUser.cs

--- TaskGarden.Data/Entities/Category.cs
+++ TaskGarden.Data/Entities/Category.cs
@@ -4,4 +4,7 @@ public class Category : BaseEntity
 {
     public string Name { get; set; }
     public string Tag { get; set; }
+
+    public string UserId { get; set; }
+    public AppUser User { get; set; }
 }
