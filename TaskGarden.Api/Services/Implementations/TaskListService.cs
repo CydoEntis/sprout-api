b8b394c14974a38525059bec1473c5d560f8fbbd
Cydo <cydoentis@gmail.com>
2025-01-22 14:13:58 -0500

    Feat: Added TaskList entity

---
+++ TaskGarden.Api/Services/Implementations/TaskListService.cs
@@ -0,0 +1,8 @@
+﻿using TaskGarden.Api.Services.Contracts;
+
+namespace TaskGarden.Api.Services.Implementations;
+
+public class TaskListService : ITaskListService
+{
+    private readonly ITaskListRepository _taskListRepository;
+}
---
+++ TaskGarden.Data/Entities/TaskList.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskGarden.Data.Models;
+
+public class TaskList : BaseEntity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string UserId { get; set; }
+}
