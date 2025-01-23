0844bfe63e4790c37a9178d6ca92ae89d817f1dc
Cydo <cydoentis@gmail.com>
2025-01-23 09:32:51 -0500

    Feat: Added TaskListItem.cs entity

---
+++ TaskGarden.Data/Entities/TaskListItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskGarden.Data.Models;
+
+public class TaskListItem : BaseEntity
+{
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public int TaskListId { get; set; }
+    [ForeignKey("TaskListId")]
+    public TaskList TaskList { get; set; }
+    public int CompletedById { get; set; }
+    [ForeignKey("UserId")]
+    public AppUser User { get; set; }
+}
