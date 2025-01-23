818da90b81384a3815a86ed41654a763200c5c0e
Cydo <cydoentis@gmail.com>
2025-01-23 09:41:09 -0500

    Fix: Added missing db migrations

---
+++ TaskGarden.Data/Migrations/20250123135652_AddedRelationshipsForTaskListAndCategory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskGarden.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRelationshipsForTaskListAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskLists_UserId",
+                table: "TaskLists",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskLists_AspNetUsers_UserId",
+                table: "TaskLists",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskLists_AspNetUsers_UserId",
+                table: "TaskLists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskLists_UserId",
+                table: "TaskLists");
+        }
+    }
+}
