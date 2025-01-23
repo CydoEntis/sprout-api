670c75e5aaa224d1ee29f23e46fd36e5cb8256a5
Cydo <cydoentis@gmail.com>
2025-01-23 09:13:38 -0500

    Feat: Added Get and Set roles to UserTaskList.cs

--- TaskGarden.Data/Entities/UserTaskList.cs
+++ TaskGarden.Data/Entities/UserTaskList.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskGarden.Data.Models;
+﻿using TaskGarden.Data.Enums;
+
+namespace TaskGarden.Data.Models;
 
 public class UserTaskList : BaseEntity
 {
@@ -6,4 +8,18 @@ public class UserTaskList : BaseEntity
     public AppUser User { get; set; }
     public int TaskListId { get; set; }
     public TaskList TaskList { get; set; }
+    public string Role { get; set; }
+
+    public void SetRole(string role)
+    {
+        if (Enum.TryParse(role, out TaskListRole roleValue))
+        {
+            Role = roleValue.ToString();
+        }
+    }
+
+    public TaskListRole GetRole()
+    {
+        return (TaskListRole)Enum.Parse(typeof(TaskListRole), Role);
+    }
 }
