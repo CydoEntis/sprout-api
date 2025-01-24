2b61c6fbc62272341af1cfa4ed1ead4d974b92b1
Cydo <cydoentis@gmail.com>
2025-01-24 14:08:38 -0500

    Refactor: Updated TaskListMapping to merge first and last name into name for MemberResponseDto.cs

--- TaskGarden.Api/Configurations/TaskListMappingProfile.cs
+++ TaskGarden.Api/Configurations/TaskListMappingProfile.cs
@@ -15,19 +15,18 @@ public class TaskListMappingProfile : Profile
         CreateMap<NewTaskListResponseDto, TaskList>().ReverseMap();
         CreateMap<TaskListResponseDto, TaskList>().ReverseMap();
         CreateMap<NewTaskListRequestDto, TaskList>().ReverseMap();
-        
+
         CreateMap<TaskList, TaskListResponseDto>()
-            .ForMember(dest => dest.Members, opt => opt.MapFrom(src => 
+            .ForMember(dest => dest.Members, opt => opt.MapFrom(src =>
                 src.UserTaskLists.Select(utl => new MemberResponseDto
                 {
                     UserId = utl.User.Id,
-                    FirstName = utl.User.FirstName,
-                    LastName = utl.User.LastName
+                    Name = $"{utl.User.FirstName} {utl.User.LastName}"
                 }).ToList()));
 
         CreateMap<UserTaskList, MemberResponseDto>()
             .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
-            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.User.FirstName))
-            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName));
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src =>
+                $"{src.User.FirstName} {src.User.LastName}"));
     }
 }
