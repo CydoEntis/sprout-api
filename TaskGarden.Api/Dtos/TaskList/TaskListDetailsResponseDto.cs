609b88af2a6f791f01d384fc0c6e8a4eb392f75a
Cydo <cydoentis@gmail.com>
2025-02-07 12:34:44 -0500

    Refactor: Added id, description, and created at date to TaskListDetailsResponseDto.cs

--- TaskGarden.Api/Dtos/TaskList/TaskListDetailsResponseDto.cs
+++ TaskGarden.Api/Dtos/TaskList/TaskListDetailsResponseDto.cs
@@ -2,10 +2,13 @@
 
 public class TaskListDetailsResponseDto
 {
+    public int Id { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
     public int CompletedTasksCount { get; set; }
-    public int TotalTasks { get; set; }
+    public int TotalTasksCount { get; set; }
     public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
     public List<MemberResponseDto> Members { get; set; } = new List<MemberResponseDto>();
     public List<TaskListItemReponseDto> TaskListItems { get; set; } = new List<TaskListItemReponseDto>();
 }
