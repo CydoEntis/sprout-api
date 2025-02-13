df3fe15654023c0e6cbff2ab4bec7114004ebc89
Cydo <cydoentis@gmail.com>
2025-02-12 21:02:55 -0500

    Feat: Added TaskListDetails and TaskListItemDetail projection models for data retrieval

---
+++ TaskGarden.Data/Projections/TaskListDetails.cs
@@ -0,0 +1,16 @@
+﻿using TaskGarden.Data.Models.Member;
+
+namespace TaskGarden.Data.Projections;
+
+public class TaskListDetails
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int CompletedTasksCount { get; set; }
+    public int TotalTasksCount { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<Member> Members { get; set; } = new List<Member>();
+    public List<TaskListItemDetail> TaskListItems { get; set; } = new List<TaskListItemDetail>();
+}
---
+++ TaskGarden.Data/Projections/TaskListItemDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskGarden.Data.Projections;
+
+public class TaskListItemDetail
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+}
