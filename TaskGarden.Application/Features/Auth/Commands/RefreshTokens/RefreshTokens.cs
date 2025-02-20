12f315ef3af291d4d7474521b93cc5e707633e02
Cydo <cydoentis@gmail.com>
2025-02-19 20:38:25 -0500

    Refactor: Added RestPassword logic to Auth Features

---
+++ TaskGarden.Application/Features/Auth/Commands/RefreshTokens/RefreshTokens.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using TaskGarden.Application.Common.Constants;
+using TaskGarden.Application.Common.Exceptions;
+using TaskGarden.Application.Features.Auth.Commands.Login;
+using TaskGarden.Application.Services.Contracts;
+using TaskGarden.Domain.Entities;
+
+
+namespace TaskGarden.Application.Features.Tokens.Commands;
+
+public record RefreshTokensCommand : IRequest<RefreshTokensResponse>;
+
+public class RefreshTokensResponse : BaseResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+}
+
+public class RefreshTokensCommandHandler(
+    ICookieService cookieService,
+    ISessionService sessionService,
+    ITokenService tokenService,
+    UserManager<AppUser> userManager)
+    : IRequestHandler<RefreshTokensCommand, RefreshTokensResponse>
+{
+    public async Task<RefreshTokensResponse> Handle(RefreshTokensCommand request, CancellationToken cancellationToken)
+    {
+        var refreshToken = cookieService.Get(CookieConsts.RefreshToken);
+        if (string.IsNullOrEmpty(refreshToken))
+            throw new NotFoundException("Token not found");
+
+        var session = await sessionService.GetSessionAsync(refreshToken);
+        if (session == null)
+            throw new NotFoundException("Session not found");
+
+        var user = await userManager.FindByIdAsync(session.UserId);
+        if (user == null)
+            throw new NotFoundException("User not found");
+
+        var newAccessToken = tokenService.GenerateAccessToken(user);
+        var newRefreshToken = tokenService.GenerateRefreshToken();
+
+        await sessionService.InvalidateSessionAsync(session);
+        await sessionService.CreateSessionAsync(user.Id, newRefreshToken);
+        cookieService.Append(CookieConsts.RefreshToken, newRefreshToken.Token, true, newRefreshToken.ExpiryDate);
+
+        return new RefreshTokensResponse { Message = "Tokens refreshed successfully.", AccessToken = newAccessToken };
+    }
+}
---
+++ TaskGarden.Application/Features/Auth/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using TaskGarden.Application.Common.Constants;
+using TaskGarden.Application.Common.Exceptions;
+using TaskGarden.Application.Features.Auth.Commands.Login;
+using TaskGarden.Domain.Entities;
+
+namespace TaskGarden.Application.Features.Auth.Commands.ResetPassword;
+
+public record ResetPasswordCommand(string Email, string Token, string NewPassword) : IRequest<ResetPasswordResponse>;
+
+public class ResetPasswordResponse : BaseResponse;
+
+public class ResetPasswordCommandHandler(UserManager<AppUser> userManager)
+    : IRequestHandler<ResetPasswordCommand, ResetPasswordResponse>
+{
+    public async Task<ResetPasswordResponse> Handle(ResetPasswordCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user is null)
+            throw new NotFoundException(ExceptionMessages.UserNotFound);
+
+        var decodedToken = Uri.UnescapeDataString(request.Token);
+
+        var tokenIsValid = await userManager.VerifyUserTokenAsync(
+            user,
+            userManager.Options.Tokens.PasswordResetTokenProvider,
+            "ResetPassword",
+            decodedToken
+        );
+
+        if (!tokenIsValid)
+            throw new InvalidTokenException(ExceptionMessages.TokenInvalid);
+
+        var currentPasswordHash = user.PasswordHash;
+        var passwordHasher = new PasswordHasher<AppUser>();
+        var passwordVerificationResult =
+            passwordHasher.VerifyHashedPassword(user, currentPasswordHash, request.NewPassword);
+
+        if (passwordVerificationResult == PasswordVerificationResult.Success)
+            throw new AlreadyExistsException("new-password", ExceptionMessages.OldPassword);
+
+        var resetPasswordResult = await userManager.ResetPasswordAsync(user, decodedToken, request.NewPassword);
+        if (!resetPasswordResult.Succeeded)
+            throw new OperationException(ExceptionTitles.PasswordResetException,
+                ExceptionMessages.PasswordResetFailed);
+
+        return new ResetPasswordResponse() { Message = "Password has been reset." };
+    }
+}
