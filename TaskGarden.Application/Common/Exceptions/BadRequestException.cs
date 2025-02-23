d654d7defbb84fcaccb3535ddf0d7e18df97f8f4
Cydo <cydoentis@gmail.com>
2025-02-23 14:20:34 -0500

    Feat: Added a BadRequestException.cs

---
+++ TaskGarden.Application/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using TaskGarden.Application.Common.Constants;
+using TaskGarden.Application.Common.Models;
+
+namespace TaskGarden.Application.Common.Exceptions;
+
+public class BadRequestException : BaseException
+{
+    public List<ErrorField> Errors { get; set; } = new();
+
+    public BadRequestException(string title, string message, string field, string fieldMessage)
+        : base(StatusCodes.Status400BadRequest, title, message)
+    {
+        Errors.Add(new ErrorField { Field = field, Message = fieldMessage });
+    }
+}
