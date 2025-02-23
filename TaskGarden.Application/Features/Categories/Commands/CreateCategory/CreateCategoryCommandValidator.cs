d70d0b61a349a92cb9286e675b704b1d4bf4e8be
Cydo <cydoentis@gmail.com>
2025-02-23 14:56:10 -0500

    Feat: Added a reusable AuthenticatedValidator.cs to check if a user is authenticated, and added a CreateCategoryCommandValidator.cs to validate category creation.

---
+++ TaskGarden.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using TaskGarden.Application.Common.Contracts;
+using TaskGarden.Application.Features.Shared.Validators;
+using TaskGarden.Application.Services.Contracts;
+
+namespace TaskGarden.Application.Features.Categories.Commands.CreateCategory;
+
+public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+{
+    public CreateCategoryCommandValidator(ICategoryRepository categoryRepository,
+        IUserContextService userContextService)
+    {
+        Include(new AuthenticatedValidator<CreateCategoryCommand>(userContextService));
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Category name is required")
+            .MaximumLength(50).WithMessage("Category name cannot exceed 100 characters");
+
+
+        RuleFor(x => x).MustAsync(async (command, _) =>
+        {
+            var userId = userContextService.GetUserId();
+            if (userId == null) return false;
+            var existingCategory = await categoryRepository.GetByNameAsync(userId, command.Name);
+            return existingCategory == null;
+        });
+    }
+}
---
+++ TaskGarden.Application/Features/Shared/Validators/AuthenticatedValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using TaskGarden.Application.Services.Contracts;
+
+namespace TaskGarden.Application.Features.Shared.Validators;
+
+public class AuthenticatedValidator<T> : AbstractValidator<T>
+{
+    public AuthenticatedValidator(IUserContextService userContextService)
+    {
+        RuleFor(x => x)
+            .Must(_ => userContextService.GetUserId() != null)
+            .WithMessage("User is not authenticated");
+    }
+}
