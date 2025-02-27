91de90df003782e4901b5d437cab871566e14d72
Cydo <cydoentis@gmail.com>
2025-02-27 17:00:00 -0500

    Feat: Added UpdateTaskListItemCommand.cs

--- TaskGarden.Application/Features/TaskListItem/Commands/UpdateTaskListItem/UpdateTaskListItemCommand.cs
+++ TaskGarden.Application/Features/TaskListItem/Commands/UpdateTaskListItem/UpdateTaskListItemCommand.cs
@@ -1,47 +1,35 @@
 ﻿using AutoMapper;
 using FluentValidation;
 using MediatR;
+using TaskGarden.Application.Common.Contracts;
 using TaskGarden.Application.Common.Exceptions;
 using TaskGarden.Application.Features.Shared.Models;
 using TaskGarden.Application.Services.Contracts;
 
 namespace TaskGarden.Application.Features.TaskListItem.Commands.UpdateTaskListItem;
 
-public record UpdateTaskListItemCommand(string Name, string Tag) : IRequest<UpdateTaskListItemResponse>;
+public record UpdateTaskListItemCommand(int Id, string Description) : IRequest<UpdateTaskListItemResponse>;
 
 public class UpdateTaskListItemResponse : BaseResponse
 {
-    public int TaskListId { get; set; }
+    public int TaskListItemId { get; set; }
 }
 
 public class UpdateTaskListItemCommandHandler(
-    IUserContextService userContextService,
-    // IValidator<UpdateTaskListItemCommand> validator,
+    ITaskListItemRepository taskListItemRepository,
     IMapper mapper) : IRequestHandler<UpdateTaskListItemCommand, UpdateTaskListItemResponse>
 {
     public async Task<UpdateTaskListItemResponse> Handle(UpdateTaskListItemCommand request,
         CancellationToken cancellationToken)
     {
-        // TODO: Add validator file and reimplement validation logic.
+        var taskListItem = await taskListItemRepository.GetByIdAsync(request.Id);
+        if (taskListItem == null)
+            throw new NotFoundException($"Task list item with id {request.Id} was not found");
 
-        // var validationResult = await validator.ValidateAsync(request, cancellationToken);
-        // if (!validationResult.IsValid)
-        //     throw new ValidationException(validationResult.Errors);
+        taskListItem.Description = request.Description;
+        await taskListItemRepository.UpdateAsync(taskListItem);
 
-        var userId = userContextService.GetUserId();
-        if (userId == null)
-            throw new UnauthorizedException("Invalid user");
-
-        // var existingCategory = await categoryRepository.GetByNameAsync(userId, request.Name);
-        // if (existingCategory != null)
-        //     throw new ConflictException($"Category with name {request.Name} already exists");
-        //
-        //
-        // var category = mapper.Map<Domain.Entities.TaskListItem>(request);
-        // category.UserId = userId!;
-        //
-        // await categoryRepository.AddAsync(category);
         return new UpdateTaskListItemResponse()
-            { Message = $"Task list item with id {1} was deleted", TaskListId = 1 };
+            { Message = $"Task list item with id {taskListItem.Id} updated", TaskListItemId = taskListItem.Id };
     }
 }
