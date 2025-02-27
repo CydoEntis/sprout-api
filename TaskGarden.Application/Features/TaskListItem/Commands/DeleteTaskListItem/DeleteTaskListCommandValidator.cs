a39fe87fc56345acaa6f9ee90e6c10025f66fa20
Cydo <cydoentis@gmail.com>
2025-02-27 17:02:18 -0500

    Feat: Added DeleteTaskListCommandValidator.cs

---
+++ TaskGarden.Application/Features/TaskListItem/Commands/DeleteTaskListItem/DeleteTaskListCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace TaskGarden.Application.Features.TaskList.Commands.DeleteTaskListItem;
+
+public class DeleteTaskListItemCommandValidator : AbstractValidator<DeleteTaskListItemCommand>
+{
+    public DeleteTaskListItemCommandValidator()
+    {
+        RuleFor(x => x.TaskListItemId).GreaterThan(0).WithMessage("Invalid task list item ID");
+    }
+}
