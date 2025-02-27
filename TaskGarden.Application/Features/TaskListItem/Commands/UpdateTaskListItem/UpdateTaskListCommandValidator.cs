179b35bae24ed1ccaeea10090a40f6736d200032
Cydo <cydoentis@gmail.com>
2025-02-27 17:05:03 -0500

    Feat: Added UpdateTaskListCommandValidator.cs

---
+++ TaskGarden.Application/Features/TaskListItem/Commands/UpdateTaskListItem/UpdateTaskListCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace TaskGarden.Application.Features.TaskListItem.Commands.UpdateTaskListItem;
+
+public class UpdateTaskListItemCommandValidator : AbstractValidator<UpdateTaskListItemCommand>
+{
+    public UpdateTaskListItemCommandValidator()
+    {
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters");
+    }
+}
