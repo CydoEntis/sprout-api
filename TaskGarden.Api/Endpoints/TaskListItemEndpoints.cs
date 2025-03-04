be338911f94c86620743de83d4a4e5d46da9e19f
Cydo <cydoentis@gmail.com>
2025-03-04 03:34:38 -0500

    Feat: Added task list item delete and update endpoints.

--- TaskGarden.Api/Endpoints/TaskListItemEndpoints.cs
+++ TaskGarden.Api/Endpoints/TaskListItemEndpoints.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
+using TaskGarden.Application.Features.TaskList.Commands.DeleteTaskListItem;
 using TaskGarden.Application.Features.TaskListItem.Commands.CreateTaskListItem;
+using TaskGarden.Application.Features.TaskListItem.Commands.UpdateTaskListItem;
 using TaskGarden.Infrastructure.Models;
 
 namespace TaskGarden.Api.Endpoints;
@@ -20,6 +22,34 @@ public static class TaskListItemEndpoints
             .RequireAuthorization()
             .Produces(StatusCodes.Status400BadRequest)
             .Produces(StatusCodes.Status200OK);
-   
+
+        group.MapPut("/",
+                async (UpdateTaskListItemCommand command, IMediator mediator) =>
+                {
+                    var response = await mediator.Send(command);
+                    return Results.Ok(ApiResponse<UpdateTaskListItemResponse>.SuccessResponse(response));
+                })
+            .WithName("UpdateTaskListItem")
+            .RequireAuthorization()
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status403Forbidden)
+            .Produces(StatusCodes.Status404NotFound);
+
+        // Delete Category Endpoint
+        group.MapDelete("/{taskListId}",
+                async (int taskListId, IMediator mediator) =>
+                {
+                    var command = new DeleteTaskListItemCommand(taskListId);
+                    var response = await mediator.Send(command);
+                    return Results.Ok(
+                        ApiResponse<DeleteTaskListItemResponse>.SuccessResponse(response));
+                })
+            .WithName("DeleteTaskListItem")
+            .RequireAuthorization()
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status403Forbidden)
+            .Produces(StatusCodes.Status404NotFound);
     }
 }
