480f89493fabb83667cee9ded938b1603db39cb0
Cydo <cydoentis@gmail.com>
2025-03-08 17:09:42 -0500

    Feat: Configured Google auth in program cs and installed required packages.

--- TaskGarden.Api/AuthenticationServiceRegistration.cs
+++ TaskGarden.Api/AuthenticationServiceRegistration.cs
@@ -18,22 +18,30 @@ public static class AuthenticationServiceRegistration
             .AddEntityFrameworkStores<AppDbContext>();
 
         services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
             {
-                ValidateIssuer = true,
-                ValidIssuer = configuration[JwtConsts.Issuer],
-                ValidateAudience = true,
-                ValidAudience = configuration[JwtConsts.Audience],
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.Zero,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration[JwtConsts.Secret]))
-            };
-        });
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration[JwtConsts.Issuer],
+                    ValidateAudience = true,
+                    ValidAudience = configuration[JwtConsts.Audience],
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration[JwtConsts.Secret]))
+                };
+            })
+            .AddGoogle(options =>
+            {
+                options.ClientId = configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+                options.CallbackPath = "/auth/google/callback"; // Matches your redirect URI
+            });
+        ;
 
         return services;
     }
