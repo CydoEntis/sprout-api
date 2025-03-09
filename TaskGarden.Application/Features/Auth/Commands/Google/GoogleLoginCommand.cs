1ef5befb7193f8602eb2250caafeb9c8c53c21eb
Cydo <cydoentis@gmail.com>
2025-03-09 17:06:10 -0400

    Fix: Updated google auth service to isntead use post message as a redirect uri instead of an actual url.

--- TaskGarden.Application/Features/Auth/Commands/Google/GoogleLoginCommand.cs
+++ TaskGarden.Application/Features/Auth/Commands/Google/GoogleLoginCommand.cs
@@ -6,48 +6,50 @@ using TaskGarden.Application.Features.Shared.Models;
 using TaskGarden.Application.Services.Contracts;
 using TaskGarden.Domain.Entities;
 
-namespace TaskGarden.Application.Features.Auth.Commands.GoogleLogin;
-
-public record GoogleLoginCommand(string IdToken) : IRequest<GoogleLoginResponse>;
-
-public class GoogleLoginResponse : BaseResponse
+namespace TaskGarden.Application.Features.Auth.Commands.GoogleLogin
 {
-    public string AccessToken { get; set; } = string.Empty;
-}
+    public record GoogleLoginCommand(string AuthorizationCode) : IRequest<GoogleLoginResponse>;
 
-public class GoogleLoginCommandHandler(
-    UserManager<AppUser> userManager,
-    ITokenService tokenService,
-    ICookieService cookieService,
-    ISessionService sessionService,
-    IGoogleAuthService googleAuthService)
-    : IRequestHandler<GoogleLoginCommand, GoogleLoginResponse>
-{
-    public async Task<GoogleLoginResponse> Handle(GoogleLoginCommand request, CancellationToken cancellationToken)
+    public class GoogleLoginResponse : BaseResponse
     {
-        var googleUserInfo = await googleAuthService.ValidateGoogleIdTokenAsync(request.IdToken);
-        if (googleUserInfo == null)
-            throw new UnauthorizedAccessException("Invalid Google token.");
+        public string AccessToken { get; set; } = string.Empty;
+    }
 
-        var user = await userManager.FindByEmailAsync(googleUserInfo.Email);
-        if (user == null)
+    public class GoogleLoginCommandHandler(
+        UserManager<AppUser> userManager,
+        ITokenService tokenService,
+        ICookieService cookieService,
+        ISessionService sessionService,
+        IGoogleAuthService googleAuthService)
+        : IRequestHandler<GoogleLoginCommand, GoogleLoginResponse>
+    {
+        public async Task<GoogleLoginResponse> Handle(GoogleLoginCommand request, CancellationToken cancellationToken)
         {
-            user = new AppUser
+            var googleUserInfo = await googleAuthService.GetUserInfoFromCodeAsync(request.AuthorizationCode);
+
+            if (googleUserInfo == null)
+                throw new UnauthorizedAccessException("Invalid Google token.");
+
+            var user = await userManager.FindByEmailAsync(googleUserInfo.Email);
+            if (user == null)
             {
-                UserName = googleUserInfo.Email,
-                Email = googleUserInfo.Email,
-                FirstName = googleUserInfo.FirstName,
-                LastName = googleUserInfo.LastName
-            };
-            await userManager.CreateAsync(user);
+                user = new AppUser
+                {
+                    UserName = googleUserInfo.Email,
+                    Email = googleUserInfo.Email,
+                    FirstName = googleUserInfo.FirstName,
+                    LastName = googleUserInfo.LastName
+                };
+                await userManager.CreateAsync(user);
+            }
+
+            var accessToken = tokenService.GenerateAccessToken(user);
+            var refreshToken = tokenService.GenerateRefreshToken();
+
+            await sessionService.CreateSessionAsync(user.Id, refreshToken);
+            cookieService.Append(CookieConsts.RefreshToken, refreshToken.Token, true, refreshToken.ExpiryDate);
+
+            return new GoogleLoginResponse { Message = "Logged in successfully", AccessToken = accessToken };
         }
-
-        var accessToken = tokenService.GenerateAccessToken(user);
-        var refreshToken = tokenService.GenerateRefreshToken();
-
-        await sessionService.CreateSessionAsync(user.Id, refreshToken);
-        cookieService.Append(CookieConsts.RefreshToken, refreshToken.Token, true, refreshToken.ExpiryDate);
-
-        return new GoogleLoginResponse { Message = "Logged in successfully", AccessToken = accessToken };
     }
 }
--- TaskGarden.Application/Services/Contracts/IGoogleAuthService.cs
+++ TaskGarden.Application/Services/Contracts/IGoogleAuthService.cs
@@ -4,5 +4,5 @@ namespace TaskGarden.Application.Services.Contracts;
 
 public interface IGoogleAuthService
 {
-    Task<GoogleUserInfo?> ValidateGoogleIdTokenAsync(string idToken);
+    Task<GoogleUserInfo?> GetUserInfoFromCodeAsync(string authorizationCode);
 }
