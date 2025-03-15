2b258e6bb31517f9acd187a37a8afc6a65e12159
Cydo <cydoentis@gmail.com>
2025-03-15 18:28:44 -0400

    Refactor: Modified LoginCommandHandler to use ValidationService.

--- TaskGarden.Application/Features/Auth/Commands/Login/LoginCommand.cs
+++ TaskGarden.Application/Features/Auth/Commands/Login/LoginCommand.cs
@@ -1,12 +1,9 @@
-﻿using FluentValidation;
-using MediatR;
-using Microsoft.AspNetCore.Identity;
+﻿using MediatR;
 using TaskGarden.Application.Common.Constants;
 using TaskGarden.Application.Common.Exceptions;
 using TaskGarden.Application.Features.Shared.Models;
 using TaskGarden.Application.Services.Contracts;
 using TaskGarden.Domain.Entities;
-using ValidationException = FluentValidation.ValidationException;
 
 namespace TaskGarden.Application.Features.Auth.Commands.Login;
 
@@ -20,22 +17,15 @@ public class LoginResponse : BaseResponse
 public class LoginCommandHandler(
     IUserService userService,
     IAuthSessionService authSessionService,
-    IValidator<LoginCommand> validator)
+    IValidationService validationService)
     : IRequestHandler<LoginCommand, LoginResponse>
 {
     public async Task<LoginResponse> Handle(LoginCommand request, CancellationToken cancellationToken)
     {
-        await ValidateRequestAsync(request, cancellationToken);
+        await validationService.ValidateRequestAsync(request, cancellationToken);
         var user = await userService.GetUserByEmailAsync(request.Email);
         var accessToken = await authSessionService.GenerateAndStoreTokensAsync(user);
 
         return new LoginResponse { Message = "Logged in successfully", AccessToken = accessToken };
-    }
-
-    private async Task ValidateRequestAsync(LoginCommand request, CancellationToken cancellationToken)
-    {
-        var validationResult = await validator.ValidateAsync(request, cancellationToken);
-        if (!validationResult.IsValid)
-            throw new ValidationException(validationResult.Errors);
     }
 }
