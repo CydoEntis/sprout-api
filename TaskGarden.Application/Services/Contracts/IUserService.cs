f0d4c0b42af126f7fd75dcf030af563453568287
Cydo <cydoentis@gmail.com>
2025-03-15 17:49:56 -0400

    Refactor: Moved User related logic to UserService for more maintainable and readable code.

---
+++ TaskGarden.Application/Services/Contracts/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using TaskGarden.Domain.Entities;
+
+namespace TaskGarden.Application.Services.Contracts;
+
+public interface IUserService
+{
+    Task<AppUser> GetUserByEmailAsync(string email);
+}
---
+++ TaskGarden.Infrastructure/Services/Identity/UserService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TaskGarden.Application.Common.Exceptions;
+using TaskGarden.Application.Services.Contracts;
+using TaskGarden.Domain.Entities;
+
+namespace TaskGarden.Infrastructure.Services.Identity;
+
+public class UserService(UserManager<AppUser> userManager) : IUserService
+{
+    public async Task<AppUser> GetUserByEmailAsync(string email)
+    {
+        return await userManager.FindByEmailAsync(email)
+               ?? throw new NotFoundException("User not found.");
+    }
+}
