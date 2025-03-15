234d566602c426bb4f50486bb652637f8660f5f2
Cydo <cydoentis@gmail.com>
2025-03-15 18:43:12 -0400

    Refactor: Updated MailKitService.cs SendEmailAsync to also take in a template.

--- TaskGarden.Application/Services/Contracts/IEmailService.cs
+++ TaskGarden.Application/Services/Contracts/IEmailService.cs
@@ -1,7 +1,7 @@
 ﻿namespace TaskGarden.Application.Services.Contracts;
 
-
 public interface IEmailService
 {
-    Task SendEmailAsync(string preview, string toEmail, string subject, string body);
+    Task SendEmailAsync(string preview, string toEmail, string subject, string templateName,
+        Dictionary<string, string> placeholders);
 }
--- TaskGarden.Infrastructure/Services/Email/MailKitService.cs
+++ TaskGarden.Infrastructure/Services/Email/MailKitService.cs
@@ -1,52 +1,57 @@
 ﻿using Microsoft.Extensions.Configuration;
 using MimeKit;
-using TaskGarden.Application.Common.Constants;
 using TaskGarden.Application.Services.Contracts;
 using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+using System.Collections.Generic;
+using System;
 
-namespace TaskGarden.Infrastructure.Services.Email;
-
-public class MailKitService : IEmailService
+namespace TaskGarden.Infrastructure.Services.Email
 {
-    private readonly string _smtpHost;
-    private readonly int _smtpPort;
-    private readonly string _email;
-    private readonly string _password;
-
-    public MailKitService(IConfiguration configuration)
+    public class MailKitService : IEmailService
     {
-        _smtpHost = configuration[SMTPConsts.SmtpHost];
-        _smtpPort = int.Parse(configuration[SMTPConsts.SmtpPort]);
-        _email = configuration[SMTPConsts.Email];
-        _password = configuration[SMTPConsts.Password];
-    }
+        private readonly string _smtpHost;
+        private readonly int _smtpPort;
+        private readonly string _email;
+        private readonly string _password;
+        private readonly IEmailTemplateService _emailTemplateService;
 
-
-    public async Task SendEmailAsync(string preview, string toEmail, string subject, string body)
-    {
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress(preview, _email));
-        message.To.Add(new MailboxAddress("", toEmail));
-        message.Subject = subject;
-
-        var bodyBuilder = new BodyBuilder
+        public MailKitService(IConfiguration configuration, IEmailTemplateService emailTemplateService)
         {
-            HtmlBody = body 
-        };
-        message.Body = bodyBuilder.ToMessageBody();
+            _smtpHost = configuration["SMTP:Host"];
+            _smtpPort = int.Parse(configuration["SMTP:Port"]);
+            _email = configuration["SMTP:Email"];
+            _password = configuration["SMTP:Password"];
+            _emailTemplateService = emailTemplateService;
+        }
 
-        using (var smtpClient = new SmtpClient())
+        public async Task SendEmailAsync(string preview, string toEmail, string subject, string templateName, Dictionary<string, string> placeholders)
         {
-            try
+            var emailBody = _emailTemplateService.GetEmailTemplate(templateName, placeholders);
+            
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(preview, _email));
+            message.To.Add(new MailboxAddress("", toEmail));
+            message.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder
             {
-                await smtpClient.ConnectAsync(_smtpHost, _smtpPort, false);
-                await smtpClient.AuthenticateAsync(_email, _password);
-                await smtpClient.SendAsync(message);
-                await smtpClient.DisconnectAsync(true);
-            }
-            catch (Exception ex)
+                HtmlBody = emailBody
+            };
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using (var smtpClient = new SmtpClient())
             {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+                try
+                {
+                    await smtpClient.ConnectAsync(_smtpHost, _smtpPort, false);
+                    await smtpClient.AuthenticateAsync(_email, _password);
+                    await smtpClient.SendAsync(message);
+                    await smtpClient.DisconnectAsync(true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
             }
         }
     }
