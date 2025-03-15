cf6a0ca88f16be563e0a77dc35377b74d9a6d4e3
Cydo <cydoentis@gmail.com>
2025-03-15 18:36:45 -0400

    Refactor: Moved all change password related logic to UserService and added a GetUserByIdAsync method.

--- TaskGarden.Application/Services/Contracts/IUserService.cs
+++ TaskGarden.Application/Services/Contracts/IUserService.cs
@@ -4,7 +4,10 @@ namespace TaskGarden.Application.Services.Contracts;
 
 public interface IUserService
 {
+    Task<AppUser> GetUserByIdAsync(string userId);
     Task CreateUserAsync(AppUser user);
     Task<AppUser> GetUserByEmailAsync(string email);
     Task<string> GeneratePasswordResetTokenAsync(AppUser user);
+    Task ValidateCurrentPasswordAsync(AppUser user, string currentPassword);
+    Task ChangeUserPasswordAsync(AppUser user, string currentPassword, string newPassword);
 }
--- TaskGarden.Infrastructure/Services/Identity/UserService.cs
+++ TaskGarden.Infrastructure/Services/Identity/UserService.cs
@@ -7,19 +7,49 @@ namespace TaskGarden.Infrastructure.Services.Identity;
 
 public class UserService(UserManager<AppUser> userManager) : IUserService
 {
+    public async Task<AppUser> GetUserByIdAsync(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        return user ?? throw new NotFoundException("User not found.");
+    }
+
     public async Task<AppUser> GetUserByEmailAsync(string email)
     {
         return await userManager.FindByEmailAsync(email)
                ?? throw new NotFoundException("User not found.");
     }
-    
+
     public async Task CreateUserAsync(AppUser user)
     {
         await userManager.CreateAsync(user);
     }
-    
+
     public async Task<string> GeneratePasswordResetTokenAsync(AppUser user)
     {
         return await userManager.GeneratePasswordResetTokenAsync(user);
+    }
+
+
+    public async Task ValidateCurrentPasswordAsync(AppUser user, string currentPassword)
+    {
+        var isValid = await userManager.CheckPasswordAsync(user, currentPassword);
+        if (!isValid)
+        {
+            throw new BadRequestException(
+                "Invalid Password",
+                "An invalid password was entered",
+                "current-password",
+                "Current password does not match."
+            );
+        }
+    }
+
+    public async Task ChangeUserPasswordAsync(AppUser user, string currentPassword, string newPassword)
+    {
+        var result = await userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        if (!result.Succeeded)
+        {
+            throw new ResourceModificationException("Failed to change the password.");
+        }
     }
 }
