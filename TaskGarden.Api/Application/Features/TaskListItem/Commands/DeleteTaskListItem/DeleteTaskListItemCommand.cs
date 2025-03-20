45339f167e2466f4fdacc7411958d6fbfdec5e9a
Cydo <cydoentis@gmail.com>
2025-03-19 21:18:31 -0400

    Refactor: Updated DeleteTaskListItemCommand.cs to no longer depend on repositories or use a validator.

--- TaskGarden.Api/Application/Features/TaskListItem/Commands/DeleteTaskListItem/DeleteTaskListItemCommand.cs
+++ TaskGarden.Api/Application/Features/TaskListItem/Commands/DeleteTaskListItem/DeleteTaskListItemCommand.cs
@@ -1,11 +1,14 @@
 ﻿using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TaskGarden.Api.Application.Shared.Handlers;
 using TaskGarden.Application.Common.Contracts;
 using TaskGarden.Application.Common.Exceptions;
 using TaskGarden.Application.Features.Shared.Models;
-using TaskGarden.Application.Services.Contracts;
+using TaskGarden.Domain.Enums;
+using TaskGarden.Infrastructure;
 
-namespace TaskGarden.Application.Features.TaskList.Commands.DeleteTaskListItem;
+namespace TaskGarden.Api.Application.Features.TaskListItem.Commands.DeleteTaskListItem;
 
 public record DeleteTaskListItemCommand(int TaskListItemId) : IRequest<DeleteTaskListItemResponse>;
 
@@ -14,36 +17,69 @@ public class DeleteTaskListItemResponse : BaseResponse
     public int TaskListId { get; set; }
 }
 
-public class DeleteTaskListItemCommandHandler(
-    IUserContextService userContextService,
-    ITaskListItemRepository taskListItemRepository
-    // IValidator<DeleteTaskListItemCommand> validator
-) : IRequestHandler<DeleteTaskListItemCommand, DeleteTaskListItemResponse>
+public class DeleteTaskListItemCommandHandler : AuthRequiredHandler,
+    IRequestHandler<DeleteTaskListItemCommand, DeleteTaskListItemResponse>
 {
+    private readonly AppDbContext _context;
+
+    public DeleteTaskListItemCommandHandler(IHttpContextAccessor httpContextAccessor, AppDbContext context)
+        : base(httpContextAccessor)
+    {
+        _context = context;
+    }
+
     public async Task<DeleteTaskListItemResponse> Handle(DeleteTaskListItemCommand request,
         CancellationToken cancellationToken)
     {
-        // var validationResult = await validator.ValidateAsync(request, cancellationToken);
-        // if (!validationResult.IsValid)
-        //     throw new ValidationException(validationResult.Errors);
+        var userId = GetAuthenticatedUserId();
 
-        var userId = userContextService.GetAuthenticatedUserId();
-        if (userId == null)
-            throw new UnauthorizedAccessException("User not authenticated");
-
-        var taskListItem = await taskListItemRepository.GetByIdAsync(request.TaskListItemId);
+        var taskListItem = await CheckIfTaskListItemExists(request.TaskListItemId);
         if (taskListItem == null)
             throw new NotFoundException("Task list item not found.");
 
+        var taskList = await CheckIfTaskListExists(taskListItem.TaskListId);
+        if (taskList == null)
+            throw new NotFoundException("Task list not found.");
 
-        var result = await taskListItemRepository.DeleteAsync(taskListItem.Id);
-        if (!result)
-            throw new ResourceModificationException("Category could not be deleted.");
+        if (!await CheckIfUserIsOwnerOrEditor(userId, taskList.Id))
+            throw new UnauthorizedAccessException("You are not authorized to delete items from this task list.");
+
+
+        if (!await DeleteTaskListItem(taskListItem))
+            throw new ResourceModificationException("Task list item could not be deleted.");
 
         return new DeleteTaskListItemResponse()
         {
-            Message = $"Item with id: {taskListItem.Id}  has been deleted successfully",
+            Message = $"Item with id: {taskListItem.Id} has been deleted successfully.",
             TaskListId = taskListItem.TaskListId
         };
+    }
+
+    private async Task<Domain.Entities.TaskListItem?> CheckIfTaskListItemExists(int taskListItemId)
+    {
+        return await _context.TaskListItems
+            .FirstOrDefaultAsync(tli => tli.Id == taskListItemId);
+    }
+
+    private async Task<Domain.Entities.TaskList?> CheckIfTaskListExists(int taskListId)
+    {
+        return await _context.TaskLists
+            .FirstOrDefaultAsync(tl => tl.Id == taskListId);
+    }
+
+    private async Task<bool> CheckIfUserIsOwnerOrEditor(string userId, int taskListId)
+    {
+        return await _context.TaskListMembers
+            .AnyAsync(q =>
+                q.TaskListId == taskListId &&
+                q.UserId == userId &&
+                (q.Role == TaskListRole.Owner || q.Role == TaskListRole.Editor));
+    }
+
+    private async Task<bool> DeleteTaskListItem(Domain.Entities.TaskListItem taskListItem)
+    {
+        _context.TaskListItems.Remove(taskListItem);
+        var result = await _context.SaveChangesAsync();
+        return result > 0;
     }
 }
