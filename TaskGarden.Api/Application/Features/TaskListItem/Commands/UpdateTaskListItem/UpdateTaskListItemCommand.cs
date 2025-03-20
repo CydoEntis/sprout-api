230b79defa864cf9a2369f5a922df93d9e402abc
Cydo <cydoentis@gmail.com>
2025-03-19 23:25:38 -0400

    Refactor: Updated UpdateTaskListItemCommand.cs to not rely on repositories.

--- TaskGarden.Api/Application/Features/TaskListItem/Commands/UpdateTaskListItem/UpdateTaskListItemCommand.cs
+++ TaskGarden.Api/Application/Features/TaskListItem/Commands/UpdateTaskListItem/UpdateTaskListItemCommand.cs
@@ -1,10 +1,13 @@
 ﻿using AutoMapper;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TaskGarden.Api.Application.Shared.Handlers;
 using TaskGarden.Application.Common.Contracts;
 using TaskGarden.Application.Common.Exceptions;
 using TaskGarden.Application.Features.Shared.Models;
 using TaskGarden.Application.Services.Contracts;
+using TaskGarden.Infrastructure;
 
 namespace TaskGarden.Application.Features.TaskListItem.Commands.UpdateTaskListItem;
 
@@ -15,21 +18,41 @@ public class UpdateTaskListItemResponse : BaseResponse
     public int TaskListId { get; set; }
 }
 
-public class UpdateTaskListItemCommandHandler(
-    ITaskListItemRepository taskListItemRepository,
-    IMapper mapper) : IRequestHandler<UpdateTaskListItemCommand, UpdateTaskListItemResponse>
+public class UpdateTaskListItemCommandHandler : AuthRequiredHandler,
+    IRequestHandler<UpdateTaskListItemCommand, UpdateTaskListItemResponse>
 {
+    private readonly AppDbContext _context;
+
+    public UpdateTaskListItemCommandHandler(IHttpContextAccessor httpContextAccessor, AppDbContext context) : base(
+        httpContextAccessor)
+    {
+        _context = context;
+    }
+
     public async Task<UpdateTaskListItemResponse> Handle(UpdateTaskListItemCommand request,
         CancellationToken cancellationToken)
     {
-        var taskListItem = await taskListItemRepository.GetByIdAsync(request.Id);
+        var taskListItem = await GetTaskListItemByIdAsync(request.Id);
         if (taskListItem == null)
             throw new NotFoundException($"Task list item with id {request.Id} was not found");
 
         taskListItem.Description = request.Description;
-        await taskListItemRepository.UpdateAsync(taskListItem);
+        if (!await UpdateTaskListItemAsync(taskListItem))
+            throw new ResourceModificationException($"Task list item with id: {taskListItem.Id} could not be updated.");
 
         return new UpdateTaskListItemResponse()
             { Message = $"Task list item with id {taskListItem.Id} updated", TaskListId = taskListItem.TaskListId };
+    }
+
+    private async Task<Domain.Entities.TaskListItem?> GetTaskListItemByIdAsync(int taskListItemId)
+    {
+        return await _context.TaskListItems.FirstOrDefaultAsync(q => q.Id == taskListItemId);
+    }
+
+    private async Task<bool> UpdateTaskListItemAsync(Domain.Entities.TaskListItem taskListItem)
+    {
+        _context.Update(taskListItem);
+        var changes = await _context.SaveChangesAsync();
+        return changes > 0;
     }
 }
