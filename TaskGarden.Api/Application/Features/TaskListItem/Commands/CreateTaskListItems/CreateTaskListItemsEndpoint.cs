f369f1e308e4e813f9aeed556927b052e7b29f99
Cydo <cydoentis@gmail.com>
2025-03-21 23:45:46 -0400

    Refactor: Updated incorrect naming for CreateTaskListItemsEndpoint.cs

---
+++ TaskGarden.Api/Application/Features/TaskListItem/Commands/CreateTaskListItems/CreateTaskListItemsEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using TaskGarden.Api.Application.Features.TaskListItem.Commands.CreateTaskListItem;
+using TaskGarden.Api.Application.Shared.Models;
+
+namespace TaskGarden.Api.Application.Features.TaskListItem.Commands.CreateTaskListItems;
+
+public static class CreateTaskListItemsEndpoint
+{
+    public static void MapCreateTaskListItemsEndpoint(this IEndpointRouteBuilder routes)
+    {
+        routes.MapPost("/api/task-list/{taskListId}/items",
+                async (CreateTaskListItemsCommand command, IMediator mediator) =>
+                {
+                    var response = await mediator.Send(command);
+                    return Results.Ok(ApiResponse<CreateTaskListItemsResponse>.SuccessWithData(response));
+                })
+            .WithName("CreateTaskListItems")
+            .WithTags("Task List Item")
+            .RequireAuthorization()
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status200OK);
+    }
+}
