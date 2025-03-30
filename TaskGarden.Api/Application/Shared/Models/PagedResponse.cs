5d0ce7528aa916712adc0f4c94be7f08a1b2dc4f
Cydo <cydoentis@gmail.com>
2025-03-29 23:07:51 -0400

    Feat: Added more properties to pagination to give the client more control.

--- TaskGarden.Api/Application/Shared/Models/PagedResponse.cs
+++ TaskGarden.Api/Application/Shared/Models/PagedResponse.cs
@@ -8,9 +8,13 @@
         public int TotalRecords { get; set; }
         public int TotalPages => (int)Math.Ceiling((double)TotalRecords / PageSize);
 
+        public bool HasNextPage => Page < TotalPages;
+        public bool HasPreviousPage => Page > 1;
+
+        public int? NextPage => HasNextPage ? Page + 1 : (int?)null;
+        public int? PreviousPage => HasPreviousPage ? Page - 1 : (int?)null;
 
         public PagedResponse(List<T> items, int page, int pageSize, int totalRecords)
-
         {
             Items = items;
             Page = page;
