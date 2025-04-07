65e1a8c7ced04371efd3775a7511d3dab49ac330
Cydo <cydoentis@gmail.com>
2025-04-07 16:49:59 -0400

    Refactor: Added optional due date to TaskListItem.cs and TasklistItemDetail.cs

--- TaskGarden.Api/Domain/Entities/TaskListItem.cs
+++ TaskGarden.Api/Domain/Entities/TaskListItem.cs
@@ -7,10 +7,10 @@ public class TaskListItem : BaseEntity
     public string Description { get; set; }
     public bool IsCompleted { get; set; } = false;
     public int Position { get; set; }
+
+    public DateTime? DueDate { get; set; }
     public required int TasklistId { get; set; }
-    [ForeignKey("TasklistId")]
-    public TaskList TaskList { get; set; }
+    [ForeignKey("TasklistId")] public TaskList TaskList { get; set; }
     public string? CompletedById { get; set; }
-    [ForeignKey("CompletedById")]
-    public AppUser? CompletedBy { get; set; }
+    [ForeignKey("CompletedById")] public AppUser? CompletedBy { get; set; }
 }
